cced7b4a9dcca0c0dcb72e26dfdfc03a9c2a053e
holey1001 <r.maeda@pon3.dev>
2023-05-25 00:48:06 +0000

    新規登録画面の実装
    - ストロングパラメータの設定
    - DBへの登録処理
    - 表示画面はまだ

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,10 +9,22 @@ class QuestionsController < ApplicationController
   
   # 質問の作成
   def new
+    @question = Question.new
   end
   
   # 質問の登録
   def create
+    # p question_params  # pメソッドで標準出力ができる
+    
+    # Questionモデルを初期化
+    @question = Question.new(question_params)
+    
+    # QuestionモデルをDBに保存
+    @question.save
+    
+    # showへリダイレクト
+    redirect_to @question  # ここでパスを指定しなくてもrailsがよしなにやってくれる
+    
   end
   
   # 質問の編集
@@ -25,5 +37,11 @@ class QuestionsController < ApplicationController
   
   # 質問の削除
   def destoroy
+  end
+  
+  private
+  # ストロングパラメータの設定で、意図していないカラムの更新が来ても無視してくれる
+  def question_params
+    params.require(:question).permit(:title, :name, :content)
   end
 end
